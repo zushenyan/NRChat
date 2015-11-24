6f3ef0de849abefe6668e060a360eb3319215c43
Andrew Yan <zushenyan@gmail.com>
2015-11-24 23:29:27 +0800

    fixed port

--- server/server.js
+++ server/server.js
@@ -2,10 +2,12 @@ var express = require("express");
 
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.get("/", function(req, res, next){
 	res.send("hello");
 });
 
-app.listen(process.env.port, function(){
+app.listen(port, function(){
 	console.log("server is running");
 });
