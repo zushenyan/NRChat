8600803d0225cb16ea13b822835a0917bec2f598
Andrew Yan <zushenyan@gmail.com>
2015-11-26 15:15:01 +0800

    test redis

--- server/server.js
+++ server/server.js
@@ -1,11 +1,21 @@
 var express = require("express");
+var db = require("redis").createClient(process.env.REDIS_URL || null);
 
 var app = express();
+
+db.on("err", function(err){
+	console.log(err);
+});
+
+db.on("connect", function(err, reply){
+	console.log("connect to db");
+});
 
 var port = process.env.PORT || 8080;
 
 app.get("/", function(req, res, next){
 	res.send("hello");
+	db.set(["test", "does it work?"]);
 });
 
 app.listen(port, function(){
